0959b7e096070c162807f13f1f8ba39cd79a7d7b
huyquang2710 <huyquang271099@gmail.com>
2023-07-05 15:21:28 +0700

    add bookdetail

--- src/main/java/com/book/controller/HomeController.java
+++ src/main/java/com/book/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.book.controller;
 
+import java.security.Principal;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
@@ -7,6 +9,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
@@ -178,5 +181,26 @@ public class HomeController {
 
 		model.addAttribute("bookList", bookList);
 		return "bookshelf";
+	}
+
+	@GetMapping("/bookDetail")
+	public String bookdetail(Model model, @PathParam("id") Long id, Principal principal) {
+		List<Book> bookList = bookService.findAll();
+
+		if(principal != null) {
+			String username = principal.getName();
+			User user = userService.findByUsername(username);
+			model.addAttribute("user", user);
+		}
+		
+		Book book = bookService.findOne(id);
+		
+		model.addAttribute("book", book);
+		
+		List<Integer> qtyList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		
+		model.addAttribute("qtyList", qtyList);
+		model.addAttribute("qty", 1);
+		return "bookDetail";
 	}
 }
