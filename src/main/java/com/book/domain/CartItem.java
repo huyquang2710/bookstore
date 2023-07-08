f6c83c5fe9f1f6b75874e52b8b454cdbcae42c0f
huyquang2710 <huyquang271099@gmail.com>
2023-07-08 12:54:56 +0700

    ShoppingCard Entity

---
+++ src/main/java/com/book/domain/CartItem.java
@@ -0,0 +1,96 @@
+package com.book.domain;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class CartItem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private int Qty;
+	private BigDecimal subtotal;
+
+	@OneToOne
+	private Book book;
+
+	@OneToMany(mappedBy = "cartItem")
+	@JsonIgnore
+	private List<BookToCartItem> bookToCardItems;
+
+	@ManyToOne
+	@JoinColumn(name = "shopping_card_id")
+	private ShoppingCart shoppingCart;
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQty() {
+		return Qty;
+	}
+
+	public void setQty(int qty) {
+		Qty = qty;
+	}
+
+	public BigDecimal getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(BigDecimal subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public List<BookToCartItem> getBookToCardItems() {
+		return bookToCardItems;
+	}
+
+	public void setBookToCardItems(List<BookToCartItem> bookToCardItems) {
+		this.bookToCardItems = bookToCardItems;
+	}
+
+	public ShoppingCart getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(ShoppingCart shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+}
